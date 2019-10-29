79e588169cb8c4d6538eca9bd717302e375b4403
Maciej Olearczyk <maciej.olearczyk@spot.pl>
2019-10-29 22:47:06 +0100

    Proguard configuration setup

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,8 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+-keep class com.molearczyk.spaceexplorer.network.nasa.** {*;}
+
+-dontwarn kotlin.reflect.**
+-keep class kotlin.** { *; }
--- app/src/main/java/com/molearczyk/spaceexplorer/network/nasa/NasaNetworkModels.kt
+++ app/src/main/java/com/molearczyk/spaceexplorer/network/nasa/NasaNetworkModels.kt
@@ -1,33 +1,36 @@
 package com.molearczyk.spaceexplorer.network.nasa
 
-data class CollectionContainer(val collection: CollectionContent)
+import com.fasterxml.jackson.annotation.JsonCreator
 
-data class CollectionContent(val href: String,
+data class CollectionContainer @JsonCreator constructor(val collection: CollectionContent)
+
+data class CollectionContent @JsonCreator constructor(val href: String,
                              val items: List<CollectionItem>,
                              val links: List<Link>?,
                              val metadata: Metadata?,
                              val version: String)
 
-data class CollectionItem(val data: List<DataItem>,
-                          val href: String,
-                          val links: List<Link>)
+data class CollectionItem @JsonCreator constructor(val data: List<DataItem>,
+                                                   val href: String,
+                                                   val links: List<Link>)
 
-data class Link(val href: String,
-                val rel: String,
-                val render: String?,
-                val prompt: String?)
+data class Link @JsonCreator constructor(val href: String,
+                                         val rel: String,
+                                         val render: String?,
+                                         val prompt: String?)
 
-data class Metadata(val total_hits: Int)
+data class Metadata @JsonCreator constructor(val total_hits: Int)
 
-data class DataItem(val center: String?,
-                    val date_created: String,
-                    val description: String?,
-                    val keywords: List<String>?,
-                    val media_type: String,
-                    val nasa_id: String,
-                    val photographer: String?,
-                    val title: String,
-                    val location: String?,
-                    val description_508: String?,
-                    val secondary_creator: String?,
-                    val album: List<String>?)
+
+data class DataItem @JsonCreator constructor(val center: String?,
+                                             val date_created: String,
+                                             val description: String?,
+                                             val keywords: List<String>?,
+                                             val media_type: String,
+                                             val nasa_id: String,
+                                             val photographer: String?,
+                                             val title: String,
+                                             val location: String?,
+                                             val description_508: String?,
+                                             val secondary_creator: String?,
+                                             val album: List<String>?)
